6c8e0f215ca94743bcf4604d45d76ee824c36088
Jordan Walker <jordanswalker28@gmail.com>
2021-05-23 19:12:07 +0100

    Initial Commit

---
+++ Assets/Scripts/BackGroundMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMusic : MonoBehaviour
+{
+    private static BackGroundMusic backGroundMusic;
+
+    private void Awake()
+    {
+        if(backGroundMusic == null)
+        {
+            backGroundMusic = this;
+            DontDestroyOnLoad(backGroundMusic);
+        }else{
+            Destroy(gameObject);
+        }
+    }
+}
